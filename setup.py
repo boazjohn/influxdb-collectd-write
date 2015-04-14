535a86cbe9cb91cce90135070ff29c9dfc5b3fc8
bob <boaz.john@juspay.in>
2015-04-14 12:50:49 +0530

    Fix: setup scripts

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 
 version = '0.1'
 
+# Forking - using 0.8 legacy client
 setup(name='influxdb-collectd-write',
       version=version,
       description='Collectd Python plugin for writing to InfluxDB',
@@ -11,7 +12,7 @@ setup(name='influxdb-collectd-write',
       keywords='',
       author='Jason Kölker',
       author_email='jason@koelker.net',
-      url='https://github.com/jkoelker/influxdb-collectd-write',
+      url='https://github.com/boazjohn/influxdb-collectd-write.git',
       license='MIT',
       py_modules=['write_influxdb'],
       include_package_data=True,
